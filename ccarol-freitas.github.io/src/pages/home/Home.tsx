66b187f5dfac565abdfe4972e54aa77db47ebb07
Caroline Freitas <carolfreitas101996@gmail.com>
2023-07-08 21:32:29 -0300

    feat: create header

--- ccarol-freitas.github.io/src/pages/home/Home.tsx
+++ ccarol-freitas.github.io/src/pages/home/Home.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import { Container } from "./HomeStyles";
-import Content from "ui/components/Content/Content";
+import Header from "./components/Header/Header";
 
 const Home = () => {
-  const component =   <h1>New page</h1>
   return(
   <Container>
-    <Content  children={component}/>
+    <Header/>
   </Container>
 );
 }
