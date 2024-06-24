5aa3bed0e129a9a07cacddbcc1a5a99b1b2918c1
ccarol-freitas <carolfreitas101996@gmail.com>
2024-06-24 12:02:08 -0300

    fix: remove backgroundImage of array

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       setBackgroundImage(ByographAvatar);
     }
     setRenderComponent(null);
-  }, [backgroundImage]);
+  }, []);
 
   const handleTabClicked = (tab: string) => {
     setCurrentTab(tab);
